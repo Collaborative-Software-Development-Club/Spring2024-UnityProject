cefdce7bb553f77430f47efea82b9a7220e3fdc0
holmes301 <quintonrholmes30@gmail.com>
2024-02-16 19:23:04 -0500

    Changed tower behavior
    Changed new tower behavior. Tower now handles its projectiles

---
+++ Tower Defense CSDC/Assets/Assets/Complete Prefabs/Ice Tower/IceTowerShooting.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceTowerShooting : MonoBehaviour
+{
+    [SerializeField] private SphereCollider detectionCollider;
+    [SerializeField] private float detectionMaximumRadius = 15f;
+    [SerializeField] private String[] tagsToCheck;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform projectileSpawn;
+    [SerializeField] private float moveSpeed = 10f;
+    private List<objData> objectsToTrack;
+    private List<GameObject> projectilePool;
+    private struct objData {
+        public GameObject obj;
+        public bool isTracking;
+    }
+
+    void Start() {
+        detectionCollider.radius = detectionMaximumRadius;
+        objectsToTrack = new List<objData>();
+        projectilePool = new List<GameObject>();
+    }
+    void Update() {
+        foreach (objData candidate in objectsToTrack.ToArray()) {
+            if (candidate.obj == null) {
+                int objIndex = objectsToTrack.IndexOf(candidate);
+                objectsToTrack.Remove(candidate); // remove nullable object + bullet
+                Destroy(projectilePool[objIndex]);
+                projectilePool.RemoveAt(objIndex);
+            }
+            else if (!candidate.isTracking) {
+                // shoot to object
+                int index = objectsToTrack.IndexOf(candidate);
+                objData tempCandidate = candidate;
+                objectsToTrack.Remove(candidate);
+                tempCandidate.isTracking = true;
+                objectsToTrack.Add(tempCandidate);
+                projectilePool.Add(Instantiate(projectilePrefab, projectileSpawn.position, Quaternion.identity));
+                
+            }
+        }
+    }
+    void FixedUpdate() {
+        foreach(GameObject proj in projectilePool.ToArray()) {
+            int index = projectilePool.IndexOf(proj);
+            moveToObject(objectsToTrack[index].obj, proj, moveSpeed);
+            if (proj.GetComponent<IceProjectileBehavior>().markForDestroy) { // for ice towers
+                GameObject temp = proj;
+                projectilePool.Remove(proj);
+                Destroy(temp);
+            }
+        }
+    }
+    void OnTriggerEnter(Collider col) {
+        foreach (String tag in tagsToCheck) {
+            if (col.gameObject.tag.Equals(tag)) { // if gameobject has a specific tag, we track it
+                objData candidate = new objData();
+                candidate.obj = col.gameObject;
+                candidate.isTracking = false;
+                objectsToTrack.Add(candidate);
+            }
+        }
+    }
+    private static void moveToObject(GameObject target, GameObject move, float speed) {
+        if (target != null) {
+            Vector3 direction = target.transform.position - move.transform.position;
+            direction.Normalize();
+            float moveDistance = speed * Time.fixedDeltaTime;
+            Vector3 newPosition = move.transform.position + (direction * moveDistance);
+            move.transform.position = newPosition;
+        }
+    }
+}
